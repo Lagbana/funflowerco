cb32c49d8ae175205b65fb766741378d554a31d4
Larry Agbana <larryagbana@gmail.com>
2021-09-18 13:20:08 -0400

    test strapi

---
+++ funflower/config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '0090c7523e18be9ec329b688be691efb'),
+    },
+  },
+});
---
+++ funflower/extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'ce9a70b6-8883-421d-99b0-a42b7de96b4b'
+};
